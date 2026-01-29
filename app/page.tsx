8bfcf9d791f56abdf85b6b753ab02994df2c4342
aarav12e <aarav12f@gmail.com>
2026-01-29 11:58:27 +0530

    env added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
-  return <div></div>
+  return <main className="main-container">
+    <section className="home-grid">
+     <div id="coin-overview">
+      <div className='header'>
+        <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin Logo" width={32} height={32} />
+      </div>
+      </div > 
+    
+    <p>Trending Coins</p>
+
+    </section>
+
+    <section className="w-full mt-7 space-y-4">
+      <p>Categories</p>
+
+    </section>
+  </main>
 }
 
 export default page
